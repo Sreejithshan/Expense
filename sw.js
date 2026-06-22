7555ab5df3b7bc4c19fce8f24bd05181e2fe50b0
Sreejithshan <142561721+Sreejithshan@users.noreply.github.com>
2026-06-22 23:09:11 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE = 'my-money-v2';
-
+const CACHE = 'my-money-v3';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['./', './index.html']))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html'])));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(resp => {
-        if (resp && resp.status === 200) {
-          const clone = resp.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-        }
-        return resp;
-      }).catch(() => cached);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+    if (resp && resp.status === 200) { const clone = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); }
+    return resp;
+  }).catch(() => caches.match('./index.html'))));
 });
